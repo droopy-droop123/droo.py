711eb5701a3796dff967d4a30b27a32ed38e0160
h0r1zon <libertsxion@gmail.com>
2017-12-13 15:58:12 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,33 +14,6 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-@bot.command()
-async def load(extension_name : str):
-    """Loads an extension."""
-    try:
-        bot.load_extension(extension_name)
-    except (AttributeError, ImportError) as e:
-        await bot.say("```py\n{}: {}\n```".format(type(e).__name__, str(e)))
-        return
-    await bot.say("{} loaded.".format(extension_name))
-
-@bot.command()
-async def unload(extension_name : str):
-    """Unloads an extension."""
-    bot.unload_extension(extension_name)
-    await bot.say("{} unloaded.".format(extension_name))
-
-@bot.command()
-async def add(left : int, right : int):
-    """Adds two numbers together."""
-    await bot.say(left + right)
-
-@bot.command()
-async def repeat(times : int, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await bot.say(content)
-
 if __name__ == "__main__":
     for extension in startup_extensions:
         try:
